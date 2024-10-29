a6d7df60b73ca95b1857333c3cf541e6be6c3085
= <nathaniel.risum2@gmail.com>
2024-10-29 15:03:16 +0100

    Nice progress

--- index.js
+++ index.js
@@ -1,16 +1,53 @@
 const app = Vue.createApp({
-    data() {
-      return {
-        count: 0
-      }
-    },
-    methods: {
+  data() {
+    return {
+      Aktier: [
+        {
+          Navn: "Maersk",
+          Koebspris: 200,
+          Salgspris: 210, 
+          Antal: 50, 
 
-    },
+        },
+        {
+          Navn: "McDonald's",
+          Koebspris: 180,
+          Salgspris: 185, 
+          Antal: 200, 
+          
+        },
+        {
+          Navn: "ToysRUs",
+          Koebspris: 200,
+          Salgspris: 5, 
+          Antal: 10, 
+          
+        },
+        {
+          Navn: "Swagster",
+          Koebspris: 200,
+          Salgspris: 300, 
+          Antal: 50, 
 
-    computed: {
-        
+        },
+      ],
+      IndexChosen: -1,
+      SelectedAmount: 1,
+      MaxStocks: 200,
+    }
+  },
+  methods: {
+
+  },
+
+  computed: {
+    SumAfKoeb() {
+
+
+      return this.Aktier[this.IndexChosen].Koebspris * this.SelectedAmount;
     },
-  })
-  
-  app.mount('#app')
+    
+  },
+})
+
+app.mount('#app')
