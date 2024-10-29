fcc7f088e1cd0532d9751c062f79dcc8e54e925b
Nathaniel Risum <nathaniel.risum2@gmail.com>
2024-10-29 20:45:55 +0100

    Added quite a lot I suppose

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
+class Transaction {
+  constructor(Mode, Navn, Koebspris, Salgspris, Antal){
+    this.Mode = Mode;
+    this.Navn = Navn;
+    this.Koebspris = Koebspris;
+    this.Salgspris = Salgspris; 
+    this.Antal = Antal; 
+  }
+}
+
 const app = Vue.createApp({
   data() {
     return {
@@ -31,20 +41,44 @@ const app = Vue.createApp({
 
         },
       ],
+      
+      isGreen: (this.TransactionMode == "Køb"),
+      isRed: this.TransactionMode == "Salg", 
+
+
+
       IndexChosen: -1,
       SelectedAmount: 1,
       MaxStocks: 200,
+      TransactionMode: null, 
+      
+      Transaktioner: []
+      
     }
   },
   methods: {
+    ProcessTransaction(){
+      t = new Transaction(this.TransactionMode, this.Aktier[this.IndexChosen].Navn, this.Aktier[this.IndexChosen].Koebspris,
+        this.Aktier[this.IndexChosen].Salgspris, this.SelectedAmount); 
+        if (this.TransactionMode == 'Køb') this.Aktier[this.IndexChosen].Antal -= this.SelectedAmount; 
+        else this.Aktier[this.IndexChosen].Antal += this.SelectedAmount; 
+      this.Transaktioner.push(t);
+    },
+
+    TransactionClass(index){
+      if (Vue.toRaw(this.Transaktioner[index].Mode) === 'Køb') return 'buyClass'; 
+      return 'saleClass';  
+    }
 
   },
 
   computed: {
     SumAfKoeb() {
-
-
       return this.Aktier[this.IndexChosen].Koebspris * this.SelectedAmount;
+    },
+    ClassForTransaction(){
+      if (this.TransactionMode == "Køb") return 'buyClass'; 
+      else if (this.TransactionMode == "Salg") return 'saleClass'; 
     },
     
   },
